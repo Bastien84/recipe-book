af6561ed9ccb1eb6cfb8aec04c7e6334623558a8
Bastien84 <bastien.geoffroy84@gmail.com>
2024-12-26 15:26:01 +0100

    v2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import Card from "./components/Card";
+const App = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const [data, setData] = useState([]);
 
-function App() {
+  const getData = async (e) => {
+    await setSearchValue(e.target.value);
+    await axios
+      .get(
+        "https://www.themealdb.com/api/json/v1/1/search.php?s=" + searchValue
+      )
+      .then((res) => {
+        setData(res.data.meals);
+        console.log(data);
+      });
+  };
+
+  const showResults = () => {
+    if (data && data.length > 0) {
+      return data.map((meal, index) => <Card key={index} meal={meal} />);
+    } else {
+      return (
+        <img
+          src="./burger.png"
+          alt=""
+          height="7972px"
+          width="7811px"
+          className="burger"
+        />
+      );
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">
+        <h1>Recipe book</h1>
+        <h2>Taste new flavors !</h2>
+        <div className="search">
+          <input
+            type="search"
+            placeholder="Ingredient, dish..."
+            value={searchValue}
+            onChange={getData}
+          />
+        </div>
+      </div>
+
+      <div className="recipes-container">{showResults()}</div>
     </div>
   );
-}
+};
 
 export default App;
